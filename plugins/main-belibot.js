0eb9803df8f6fd8668e3190515b253bfb1bcff97
Maxxy-Md <kahfilangsa@gmail.com>
2022-10-12 19:58:20 +0700

    Maxxy-V10

---
+++ plugins/main-belibot.js
@@ -0,0 +1,26 @@
+let handler = async (m, { conn }) => {
+const ftroli = {
+    key : {
+    remoteJid: '6285380166282-1614953337@g.us',
+    participant : '0@s.whatsapp.net'
+    },
+    message: {
+    orderMessage: {
+    itemCount : 2022,
+    status: 1,
+    surface : 1,
+    message: 'Get Bot', 
+    orderTitle: `Wabot`,
+    thumbnail: 'https://telegra.ph/file/8907374948af8cd672fdc.jpg', 
+    sellerJid: '0@s.whatsapp.net' 
+    }
+    }
+    }
+	conn.reply(m.chat, price1, ftroli)
+}
+
+handler.help = ['sewa', 'sewabot']
+handler.tags = ['info']
+handler.command = /^(sewa|sewabot)?$/i
+
+module.exports = handler
